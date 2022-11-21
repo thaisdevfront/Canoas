9dc4a158d0a8d652092275ec7c12866d9d6396ed
thaisdevfront <86856151+thaisdevfront@users.noreply.github.com>
2022-11-20 22:26:20 -0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
     },
@@ -6,9 +10,9 @@ var swiper = new Swiper(".mySwiper", {
   var swiper = new Swiper(".mySwiperTabs", {
           slidesPerView: 3,
           cssMode: true,
-          //  autoplay: {
-          // delay: 4500,
-          // disableOnInteraction: false,
-          // },
+           autoplay: {
+          delay: 4500,
+          disableOnInteraction: false,
+          },
           
           });
